3791939ffc1977f06c1a7afdf0055afde9da652c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-30 18:42:53 +0100

    Worked with packages

---
+++ src/com/example/packages/Packages.java
@@ -0,0 +1,12 @@
+package com.example.packages;
+
+import org.w3c.dom.Node;
+
+public class Packages {
+
+    public static void main(String[] args) {
+
+        Node node = null;
+        org.w3c.dom.Node anotherNode = null;
+    }
+}
